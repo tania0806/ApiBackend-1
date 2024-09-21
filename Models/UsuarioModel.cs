572d4f414d7b3fb618ffc364f4bffdf8b4277712
AnthonyBuenoG <anthony.eb44@gmail.com>
2024-09-21 00:26:06 -0600

    Primer Commit

---
+++ Models/UsuarioModel.cs
@@ -0,0 +1,13 @@
+using System;
+namespace reportesApi.Models
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+        public string NombreUsuario { get; set; }
+        public string NombrePersona { get; set; }
+        public string user { get; set; }
+        public string pass { get; set; }
+
+    }
+}
